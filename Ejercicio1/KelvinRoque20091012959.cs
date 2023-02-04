54fc7b20b28fa7182820f931069bcf5f79f50b91
KELVIN21ROQUE <124406014+KELVIN21ROQUE@users.noreply.github.com>
2023-02-04 04:28:45 -0600

    tarea 1
    Ejercicio de pares e impare

---
+++ Ejercicio1/KelvinRoque20091012959.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejercicio1
+{
+    public partial class KelvinRoque20091012959 : Form
+    {
+        public KelvinRoque20091012959()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int num = 0, parimpar = 0;
+            num = int.Parse(textBox1.Text);
+            parimpar = num % 2;
+
+            if (parimpar > 0)
+            {
+                textBox2.Text = "El Numero Es Impar";
+
+            }
+
+            else
+            {
+                textBox2.Text = "El Numero Es Par";
+            }
+        {
+
+        }
+    }
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
