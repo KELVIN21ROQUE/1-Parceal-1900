69d88b6fbe3b863ff437376bffca172b50cc1222
KELVIN21ROQUE <124406014+KELVIN21ROQUE@users.noreply.github.com>
2023-02-11 12:55:06 -0600

    Tarea2
    Ejercicio de promedio de notas

---
+++ Ejercicio2.Designer.cs
@@ -0,0 +1,235 @@
+﻿namespace Ejercicio2
+{
+    partial class Tarea2kelvinRoque20091012959
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.buttonlimpiar = new System.Windows.Forms.Button();
+            this.buttoncalcular = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtpromedio = new System.Windows.Forms.TextBox();
+            this.txtnota4 = new System.Windows.Forms.TextBox();
+            this.txtnota3 = new System.Windows.Forms.TextBox();
+            this.txtnota2 = new System.Windows.Forms.TextBox();
+            this.txtnota1 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtcondicion = new System.Windows.Forms.TextBox();
+            this.groupBox2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.buttonlimpiar);
+            this.groupBox2.Controls.Add(this.buttoncalcular);
+            this.groupBox2.Location = new System.Drawing.Point(531, 32);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(211, 242);
+            this.groupBox2.TabIndex = 7;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "groupBox2";
+            // 
+            // buttonlimpiar
+            // 
+            this.buttonlimpiar.Location = new System.Drawing.Point(44, 92);
+            this.buttonlimpiar.Name = "buttonlimpiar";
+            this.buttonlimpiar.Size = new System.Drawing.Size(149, 52);
+            this.buttonlimpiar.TabIndex = 1;
+            this.buttonlimpiar.Text = "Limpiar";
+            this.buttonlimpiar.UseVisualStyleBackColor = true;
+            this.buttonlimpiar.Click += new System.EventHandler(this.buttonlimpiar_Click);
+            // 
+            // buttoncalcular
+            // 
+            this.buttoncalcular.Location = new System.Drawing.Point(44, 33);
+            this.buttoncalcular.Name = "buttoncalcular";
+            this.buttoncalcular.Size = new System.Drawing.Size(149, 53);
+            this.buttoncalcular.TabIndex = 0;
+            this.buttoncalcular.Text = "Calcular";
+            this.buttoncalcular.UseVisualStyleBackColor = true;
+            this.buttoncalcular.Click += new System.EventHandler(this.buttoncalcular_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtcondicion);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.txtpromedio);
+            this.groupBox1.Controls.Add(this.txtnota4);
+            this.groupBox1.Controls.Add(this.txtnota3);
+            this.groupBox1.Controls.Add(this.txtnota2);
+            this.groupBox1.Controls.Add(this.txtnota1);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(45, 32);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(415, 235);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // txtpromedio
+            // 
+            this.txtpromedio.Location = new System.Drawing.Point(192, 144);
+            this.txtpromedio.Name = "txtpromedio";
+            this.txtpromedio.Size = new System.Drawing.Size(100, 20);
+            this.txtpromedio.TabIndex = 19;
+            // 
+            // txtnota4
+            // 
+            this.txtnota4.Location = new System.Drawing.Point(192, 115);
+            this.txtnota4.Name = "txtnota4";
+            this.txtnota4.Size = new System.Drawing.Size(100, 20);
+            this.txtnota4.TabIndex = 18;
+            // 
+            // txtnota3
+            // 
+            this.txtnota3.Location = new System.Drawing.Point(192, 89);
+            this.txtnota3.Name = "txtnota3";
+            this.txtnota3.Size = new System.Drawing.Size(100, 20);
+            this.txtnota3.TabIndex = 17;
+            // 
+            // txtnota2
+            // 
+            this.txtnota2.Location = new System.Drawing.Point(192, 61);
+            this.txtnota2.Name = "txtnota2";
+            this.txtnota2.Size = new System.Drawing.Size(100, 20);
+            this.txtnota2.TabIndex = 16;
+            // 
+            // txtnota1
+            // 
+            this.txtnota1.Location = new System.Drawing.Point(192, 36);
+            this.txtnota1.Name = "txtnota1";
+            this.txtnota1.Size = new System.Drawing.Size(100, 20);
+            this.txtnota1.TabIndex = 15;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(111, 144);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(51, 13);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "Promedio";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(54, 114);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(102, 13);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Ingrese Cuarta Nota";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(54, 89);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(108, 13);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Ingrese Tercera Nota";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(54, 64);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(114, 13);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Ingrese Segunda Nota";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(54, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(108, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Ingrese Primmer Nota";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(114, 182);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(54, 13);
+            this.label6.TabIndex = 20;
+            this.label6.Text = "Condicion";
+            // 
+            // txtcondicion
+            // 
+            this.txtcondicion.Location = new System.Drawing.Point(192, 179);
+            this.txtcondicion.Name = "txtcondicion";
+            this.txtcondicion.Size = new System.Drawing.Size(100, 20);
+            this.txtcondicion.TabIndex = 21;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button buttonlimpiar;
+        private System.Windows.Forms.Button buttoncalcular;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtpromedio;
+        private System.Windows.Forms.TextBox txtnota4;
+        private System.Windows.Forms.TextBox txtnota3;
+        private System.Windows.Forms.TextBox txtnota2;
+        private System.Windows.Forms.TextBox txtnota1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtcondicion;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ Ejercicio2.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejercicio2
+{
+    public partial class Tarea2kelvinRoque20091012959 : Form
+    {
+        public Tarea2kelvinRoque20091012959()
+        {
+            InitializeComponent();
+        }
+
+        private void buttoncalcular_Click(object sender, EventArgs e)
+        {
+            double nota_1, nota_2, nota_3, nota_4, promedio;
+            string condicion;
+            nota_1 = double.Parse(txtnota1.Text);
+            nota_2 = double.Parse(txtnota2.Text);
+            nota_3 = double.Parse(txtnota3.Text);
+            nota_4 = double.Parse(txtnota4.Text);
+
+            promedio = (nota_1 + nota_2 + nota_3 + nota_4) / 4;
+            if (promedio < 0)
+            {
+                condicion = "DESAPROBADO";
+                txtpromedio.Text = promedio.ToString("N2");
+                txtcondicion.Text = condicion;
+            }
+            else if (promedio > 15 && promedio <= 20) ;
+            {
+                condicion = "Aprobado";
+                txtpromedio.Text = promedio.ToString("N2");
+                txtcondicion.Text = condicion;
+            }
+        }
+
+        private void buttonlimpiar_Click(object sender, EventArgs e)
+        {
+            txtnota1.Clear();
+            txtnota2.Clear();
+            txtnota3.Clear();
+            txtnota4.Clear();
+            txtpromedio.Clear();
+            txtcondicion.Clear();
+            txtnota1.Focus();
+        }
+    }
+}
